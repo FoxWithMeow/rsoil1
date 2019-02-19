fe94ba1e17c71411b19e2f7249a5c213bb0456e5
Natasha <pachirisu.bna@ya.ru>
2019-02-19 09:37:39 +0300

    What with netcore...

--- rsoil1/Startup.cs
+++ rsoil1/Startup.cs
@@ -24,7 +24,7 @@ namespace rsoil1
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();//.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
